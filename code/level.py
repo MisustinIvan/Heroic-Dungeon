b4ff97c7b4a9612ce33598bd9121f3fcd89fd108
Ivan Mišustin <ivanmis596@gmail.com>
2022-07-05 22:13:33 +0200

    done for the day, commit message in obsidian

--- code/level.py
+++ code/level.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pygame, sys
+import pygame, sys, csv
 
 class Level:
     def __init__(self):
         self.running_status = True 
         self.display_surface = pygame.display.get_surface()    
 
+        self.import_csv_layout('../data/maps/test_level.csv')
+
     def import_csv_layout(self,path):
-        pass
+        with open(path, newline='\n') as csvfile:
+            csvreader = csv.reader(csvfile)
+            for shit in csvreader:
+                for shus in shit:
+                    print(shus)
 
     def draw(self):
         pass
